8f345789fd7f5259587a0f48fd56022c11d73cd5
SergiyStepanchuk <prochliste@gmail.com>
2020-12-17 11:36:34 +0200

    AddUnitTest

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,7 +1,10 @@
 ﻿#include "pch.h"
 #include "CppUnitTest.h"
+#include <fstream>
+#include "../Program/main.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace UnitTest1
 {
@@ -9,8 +12,15 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(met_get_bill_by_payer)
 		{
+			fstream stream("r.txt");
+			float sum = -1;
+			if (stream.is_open()) {
+				sum = get_bill_by_payer(stream, "ror");
+				stream.close();
+			}
+			Assert::AreEqual(sum, 334.f);
 		}
 	};
 }
